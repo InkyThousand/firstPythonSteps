8f991b870044d0f27b5dffbf21308dab6ea8c667
Pavel Losiev <inky.thousand@gmail.com>
2025-06-06 11:19:54 +0200

    Fix unreachable condition

--- taskForBeginner.py
+++ taskForBeginner.py
@@ -9,15 +9,14 @@ else:
 print("======================")
 ### 4. FizzBuzz
 for i in range(1, 101):
-    if i % 3 == 0:
-        print("Fizz")
+    if i % 3 == 0 and i % 5 == 0:
+        print("FizzBuzz")
     elif i % 5 == 0:
         print("Buzz")
-    elif i % 3 == 0 and i % 5 == 0:
-        print("FizzBuzz")
+    elif i % 3 == 0:
+        print("Fizz")
     else:
         print(i)
-
 
 print("======================")
 ### 5. Password Validator
